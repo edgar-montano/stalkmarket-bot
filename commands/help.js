bb0eac6bddccb0c468d6576c96c9d2a0d23383c7
Edgar Montano <em2532@nyu.edu>
2020-04-04 18:13:16 -0400

    added help command

---
+++ commands/help.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const { prefix } = require("../config.json");
+module.exports = {
+  name: "help",
+  args: false,
+  description: "Gives a list of the available commands.",
+  execute(message, args) {
+    const commandFiles = fs
+      .readdirSync("./commands")
+      .filter((file) => file.endsWith(".js"));
+
+    let commands = commandFiles.map(
+      (command) => prefix + command.slice(0, command.length - 3)
+    );
+    let usage = "The following commands can be used: " + commands;
+    console.log(usage);
+    // message.channel.send(usage);
+  },
+};
