a27189e7c4fc5089a6d700449aed91c69dc709ad
Edgar Montano <em2532@nyu.edu>
2020-04-05 01:11:04 -0400

    aesthetic fix for readability

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
       .filter((file) => file.endsWith(".js"));
 
     let commands = commandFiles.map(
-      (command) => prefix + command.slice(0, command.length - 3)
+      (command) => "`" + prefix + command.slice(0, command.length - 3) + "`"
     );
     let usage = "The following commands can be used: " + commands;
     message.channel.send(usage);
