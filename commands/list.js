63db14f85da87dfa96165202d8dfa44f5a4d2c61
Edgar Montano <em2532@nyu.edu>
2020-04-09 19:05:03 -0400

    cost now added to list

--- commands/list.js
+++ commands/list.js
@@ -1,5 +1,7 @@
 const loadWeek = require("../utils/loadWeek");
 const initializeDay = require("../utils/initializeDay");
+const getLastSunday = require("../utils/getLastSunday");
+const cost = require("../data/cost.json");
 module.exports = {
   name: "list",
   description: "*Usage: `!list`\nList all previous sell values for the week.*",
@@ -8,6 +10,11 @@ module.exports = {
     const username = message.author.username.slice(0, 5);
     initializeDay();
     const data = loadWeek(username);
+    let sunday = getLastSunday();
+    if (cost.hasOwnProperty(sunday)) {
+      if (cost[sunday].hasOwnProperty(username))
+        data.addField("Cost of initial purchase", cost[sunday][username]);
+    }
     message.channel.send(data);
   },
 };
