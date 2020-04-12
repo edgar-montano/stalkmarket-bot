281686f1b5570d9b9f3b6f9cd42d797b1c9505a5
Edgar Montano <em2532@nyu.edu>
2020-04-12 13:52:11 -0400

    patched cost command to only show last sunday values

--- commands/cost.js
+++ commands/cost.js
@@ -1,11 +1,15 @@
 const loadCost = require("../utils/loadCost");
+const getLastSunday = require("../utils/getLastSunday");
 module.exports = {
   name: "cost",
   description:
     "Usage: `!cost`\nLists the costs of turnips, this is only available on Sunday adds.",
   args: false,
   execute(message, args) {
-    const data = loadCost();
-    message.channel.send(data);
+    const date = new Date();
+    const lastSunday =
+      date.getDay() === 0 ? date.toDateString() : getLastSunday();
+    const data = loadCost(lastSunday);
+    message.channel.send(data.setTitle(`Cost of turnips for ${lastSunday}`));
   },
 };
