37d65a41c227144bfb316627b6dc3557931fab69
Edgar Montano <em2532@nyu.edu>
2020-04-12 14:24:52 -0400

    added get utc helper

---
+++ utils/getUTC.js
@@ -0,0 +1,15 @@
+/**
+ * Get UTC returns back a new output date in a specified timezone
+ * @param {Number} timeZoneOffset = offset of the timezone we desire
+ * @param {Date} newDate - the new date value for our timezone
+ */
+
+module.exports = getUTC = (timeZoneOffset) => {
+  if (!parseInt(timeZoneOffset)) return false;
+  const current = new Date();
+  const currentTimezone = -current.getTimezoneOffset() / 60; // convert to hours
+  const timezoneDifference = timeZoneOffset - currentTimezone;
+  const timezoneOffsetMs = timezoneDifference * 1000 * 60 * 60; //convert hours to ms
+  const newDate = new Date(current.getTime() + timezoneOffsetMs);
+  return newDate;
+};
