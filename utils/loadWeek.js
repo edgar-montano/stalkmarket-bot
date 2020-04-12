214f36da444e74b185b5cd35296b129bee385f9a
Edgar Montano <em2532@nyu.edu>
2020-04-12 18:04:09 -0400

    load week now limited

--- utils/loadWeek.js
+++ utils/loadWeek.js
@@ -9,7 +9,7 @@ const getDate = require("../utils/getDate");
 module.exports = loadWeek = (username) => {
   const date = getDate(username);
   //can only be a value 0 - 6
-  const day = date.getDay();
+  const day = date.getDay() !== 0 ? day.getDay() : 6;
   console.log(`Load week ${date} ${day}`);
   const emedData = new Discord.MessageEmbed()
     .setColor("#0099ff")
